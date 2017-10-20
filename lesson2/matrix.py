97cef34031d7904f07f8f3245685f269f2b54f01
Гончаров Павел Владимирович <pgoncharov@intervale.ru>
2017-10-21 00:57:30 +0300

    Homework files was added

---
+++ lesson2/matrix.py
@@ -0,0 +1,197 @@
+import random
+import operator
+import sys
+
+class MatrixError(BaseException):
+    """ Класс исключения для матриц """
+    pass
+
+class Matrix(object):
+    """Простой класс матрицы в Python
+    Основные операции линейной алгебры реализованы
+    путем перегрузки операторов 
+    """
+    def __init__(self, n, m, init=True):
+        """Конструктор
+
+        #Аргументы:
+            n    :  int, число строк
+            m    :  int, число столбцов 
+            init :  (необязательный параметр), логический.
+                    если False, то создается пустой массив
+        """
+        if init:
+            # создаем массив нулей
+            self.array = [[0]*m for x in range(n)]
+        else:
+            self.array = []
+
+        self.n = n
+        self.m = m
+
+    def __getitem__(self, idx):
+        """Перегрузка оператора получения элемента массива 
+        """
+        # проверяем, если индекс - это список индексов
+        if isinstance(idx, tuple): 
+            if len(idx) == 2: 
+                return self.array[idx[0]][idx[1]]
+            else:
+                # у матрицы есть только строки и столбцы
+                raise MatrixError("Matrix has only two shapes!")
+        else:
+            return self.array[idx]
+
+    def __setitem__(self, idx, item):
+        """Перегрузка оператора присваивания 
+        """
+        # проверяем, если индекс - это список индексов
+        if isinstance(idx, tuple):
+            if len(idx) == 2: 
+                self.array[idx[0]][idx[1]] = item
+            else:
+                # у матрицы есть только строки и столбцы
+                raise MatrixError("Matrix has only two shapes!")
+        else:
+            self.array[idx] = item
+
+    def __str__(self):
+        """Переопределяем метод вывода матрицы в консоль
+        """
+        s='\n'.join([' '.join([str(item) for item in row]) for row in self.array])
+        return s + '\n'
+
+    def getRank(self):
+        """Получить число строк и столбцов
+        """
+        return (self.n, self.m)
+
+
+    def __eq__(self, mat):
+        """ Проверка на равенство """
+
+        return (mat.array == self.array)
+
+    def transpose(self):
+        """ Транспонированное представление матрицы 
+    
+            Здесь нужно вписать свой код для создания 
+            функции, возвращающей транспонированную
+            матрицу
+
+            Aij = Aji
+        """
+        return self
+
+    def __add__(self, mat):
+        """ Переопределение операции сложения "+"
+        для матриц
+        """
+        if self.getRank() != mat.getRank():
+            raise MatrixError("Trying to add matrixes of varying rank!")
+
+        '''
+        Сюда вписать код функции, выполняющей
+        операцию сложения
+
+        Cij = Aij+Bij
+        '''
+        return self
+
+    def __sub__(self, mat):
+        """ Переопределение операции вычитания "-"
+        для матриц
+        """
+        if self.getRank() != mat.getRank():
+            raise MatrixError("Trying to add matrixes of varying rank!")
+
+        '''
+        Сюда вписать код функции, выполняющей
+        операцию вычитания
+
+        Cij = Aij-Bij
+        '''
+        return self
+
+    def __mul__(self, mat):
+        """Произведение Адамара или поточечное умножение"""
+        mulmat = Matrix(self.n, self.m) # результирующая матрица
+
+        # если второй аргумент - число, то 
+        # просто умножить каждый элемент на это число
+        if isinstance(mat, int) or isinstance(mat, float):
+            for i in range(self.n):
+                for j in range(self.m):
+                    mulmat[i][j] = self.array[i][j]*mat
+            return mulmat
+        else:
+            # для поточечного перемножения матриц  
+            # их размерности должны быть одинаковыми
+            if (self.n != mat.n or self.m != mat.m):
+                raise MatrixError("Matrices cannot be multipled!")
+                
+            for i in range(self.n):
+                for j in range(self.m):
+                    mulmat[i][j] = self.array[i][j]*mat[i][j]
+            return mulmat
+
+    def dot(self, mat):
+        """ Матричное умножение """
+        
+        matn, matm = mat.getRank()
+        
+        # для перемножения матриц число столбцов одной 
+        # должно равняться числу строк в другой
+        if (self.m != matn):
+            raise MatrixError("Matrices cannot be multipled!")
+        '''
+        Здесь написать код, выполняющий 
+        матричное перемножение и возвращаюший
+        новую матрицу
+
+        Cij = sum(Aik*Bkj)
+        '''
+        return self
+
+    @classmethod
+    def _makeMatrix(cls, array):
+        """Переопределение конструктора
+        """
+        n = len(array)
+        m = len(array[0])
+        # Validity check
+        if any([len(row) != m for row in array[1:]]):
+            raise MatrixError("inconsistent row length")
+        mat = Matrix(n,m, init=False)
+        mat.array = array
+
+        return mat
+
+    @classmethod
+    def fromList(cls, listoflists):
+        """ Создание матрицы напрямую из списка """
+
+        # E.g: Matrix.fromList([[1 2 3], [4,5,6], [7,8,9]])
+
+        array = listoflists[:]
+        return cls._makeMatrix(array)
+
+    @classmethod
+    def makeId(cls, n):
+        """ Создать единичную матрицу размера (nxn) """
+
+        array = [[0]*n for x in range(n)]
+        idx = 0
+        
+        for row in array:
+            row[idx] = 1
+            idx += 1
+
+        return cls.fromList(array)
+
+
+if __name__ == "__main__":
+
+    pass
+
+
---
+++ lesson2/testMatrix.py
@@ -0,0 +1,50 @@
+import unittest
+from matrix import Matrix
+
+class MatrixTests(unittest.TestCase):
+
+    def testAdd(self):
+        m1 = Matrix.fromList([[1, 2, 3], [4, 5, 6]])
+        m2 = Matrix.fromList([[7, 8, 9], [10, 11, 12]])        
+        m3 = m1 + m2
+        self.assertTrue(m3 == Matrix.fromList([[8, 10, 12], [14,16,18]]))
+
+    def testSub(self):
+        m1 = Matrix.fromList([[1, 2, 3], [4, 5, 6]])
+        m2 = Matrix.fromList([[7, 8, 9], [10, 11, 12]])        
+        m3 = m2 - m1
+        self.assertTrue(m3 == Matrix.fromList([[6, 6, 6], [6, 6, 6]]))
+
+    def testMul(self):
+        m1 = Matrix.fromList([[1, 2], [1, 2]])
+        m2 = Matrix.fromList([[2, 1], [2, 1]])
+        s1 = 3
+        self.assertTrue(m1*m2 == Matrix.fromList([[2, 2], [2, 2]]))
+        self.assertTrue(m1*s1 == Matrix.fromList([[3, 6], [3, 6]]))
+        self.assertTrue(m2*s1 == Matrix.fromList([[6, 3], [6, 3]]))
+
+
+    def testDot(self):
+        m1 = Matrix.fromList([[1, 2, 3], [4, 5, 6]])
+        m2 = Matrix.fromList([[7, 8], [10, 11], [12, 13]])
+        self.assertTrue(m1.dot(m2) == Matrix.fromList([[63, 69], [150, 165]]))
+        self.assertTrue(m2.dot(m1) == Matrix.fromList([[39, 54, 69], [54, 75, 96], [64, 89, 114]]))
+
+    def testTranspose(self):
+
+        m1 = Matrix.fromList([[1,2,3], [4, 5, 6]])
+        m2 = Matrix.fromList([[1, 4], [2, 5], [3, 6]])
+        
+        self.assertTrue(m1.transpose()==m2)
+        m2 = m1.transpose().transpose()
+        self.assertTrue(m2 == m1)
+
+    def testId(self):
+
+        m1 = Matrix.makeId(3)
+        m2 = Matrix.fromList([[1, 2, 3], [3, 4, 5], [5, 6, 7]])
+        m3 = m2.dot(m1)
+        self.assertTrue(m3 == m2)
+
+if __name__ == "__main__":
+    unittest.main()

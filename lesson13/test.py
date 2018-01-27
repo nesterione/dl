19e2810dbfd0cbefb8b29b5daf49a4df7f3b8d0d
Ihar Nestsiarenia <nesterione@gmail.com>
2018-01-27 10:07:37 +0300

    lesson 13

--- lesson13/test.py
+++ lesson13/test.py
@@ -15,7 +15,7 @@ import os
 batch_size = 32
 num_classes = 10
 epochs = 2
-data_augmentation = True
+data_augmentation = False
 num_predictions = 20
 save_dir = os.path.join(os.getcwd(), 'saved_models')
 model_name = 'keras_cifar10_trained_model.h5'
